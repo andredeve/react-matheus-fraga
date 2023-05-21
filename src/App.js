2dad87a709f766f3528b9dab02da91eef7cc9a41
andredeve <60305254+andredeve@users.noreply.github.com>
2023-05-21 18:15:58 -0400

    Props e Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-export default function App(){
+import React from "react";
+
+
+const BemVindo = (prop) =>{
   return(
     <div>
-      <h1>Bem Vindo Ao Sitema</h1>
+      <h1>Bem Vindo {prop.nome}</h1>
     </div>
-  )
+  );
 }
+
+function App(){
+  return(
+    <div>
+      <h1>Bem Vindo Ao Sistema</h1>
+      <BemVindo nome="Teste"/>
+    </div>
+  );
+}
+
+export default App;
